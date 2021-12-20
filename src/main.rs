5267a5d40381b9cd006d7c9ea21b55bc1afbe320
Aleksander Krauze <alkoszczak@gmail.com>
2021-12-20 21:36:44 +0100

    Created simple tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 mod x11;
 
-fn main() {
-    let x11 = x11::X11Connection::new().unwrap();
+use std::error::Error;
+use std::process;
+
+use tokio::runtime;
+
+async fn run() -> Result<(), Box<dyn Error>> {
+    let x11 = x11::X11Connection::new()?;
     x11.set_root_name("test");
+
+    Ok(())
+}
+
+fn main() {
+    let rt = runtime::Builder::new_current_thread()
+        .build()
+        .expect("Failed to create tokio runtime.");
+
+    let result = rt.block_on(run());
+    match result {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        }
+    }
 }
