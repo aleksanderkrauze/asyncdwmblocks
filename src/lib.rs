7a395f5b7d105133569f064b044a748005469ed5
Aleksander Krauze <alkoszczak@gmail.com>
2022-01-06 16:29:55 +0100

    Moved pipe module into utils module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod block;
 pub mod config;
-pub mod pipe;
 pub mod server;
 pub mod statusbar;
+pub mod utils;
 pub mod x11;
---
+++ src/utils/mod.rs
@@ -0,0 +1,4 @@
+pub mod pipe;
+
+pub use pipe::mpsc_pipe;
+pub use pipe::mpsc_pipe_translate;
