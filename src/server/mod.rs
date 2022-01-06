f489ac61f1143a00307a2cd7012d46bd1d02f089
Aleksander Krauze <alkoszczak@gmail.com>
2022-01-06 12:32:55 +0100

    Prepare going back to master

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,19 @@
-// mod frames;
+mod tcp;
+
+use std::sync::Arc;
 
 use async_trait::async_trait;
 use tokio::sync::mpsc;
 
+use crate::config::Config;
 use crate::statusbar::BlockRefreshMessage;
 
 #[async_trait]
 pub(self) trait Listener {
-    fn new(sender: mpsc::Sender<BlockRefreshMessage>) -> Self;
+    fn new(sender: mpsc::Sender<BlockRefreshMessage>, config: Arc<Config>) -> Self;
+    async fn run(&mut self);
+}
+
+pub enum ListenerTypes {
+    Tcp,
 }
