86555eeb27cd117be63499dcc04e21564acbf241
Aleksander Krauze <alkoszczak@gmail.com>
2022-01-09 12:48:12 +0100

    Updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! # Features
 //!
 //! - `tcp`: Enables IPC through tcp sockets.
+//! - `ipc`: Builds library with support of IPC (inter process communication).
+//! This is automatically enabled when needed and should not be manually selected.
 
 pub mod block;
 pub mod config;
